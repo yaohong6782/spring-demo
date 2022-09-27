6af5534dec94bbdd2683d028723a23ec5fda83b9
yaohong <yaohong6782@gmail.com>
2022-09-27 16:12:14 +0800

    project controller error logic

--- src/main/java/com/example/demo/web/ProjectController.java
+++ src/main/java/com/example/demo/web/ProjectController.java
@@ -1,5 +1,9 @@
 package com.example.demo.web;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.naming.Binding;
 import javax.validation.Valid;
 
@@ -7,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,8 +31,18 @@ public class ProjectController {
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 		
 		if (result.hasErrors()) {
-			return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+			Map<String, String> errorMap = new HashMap<>();
+			
+			for (FieldError error: result.getFieldErrors()) {
+				errorMap.put(error.getField(), error.getDefaultMessage());
+			}
+
+//			return new ResponseEntity<Map<String,String>>(errorMap, HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<List<FieldError>>(result.getFieldErrors(), HttpStatus.BAD_REQUEST);
+//			return new ResponseEntity<String>("Invalid", HttpStatus.BAD_REQUEST);
 		}
+		
+		
 		
 		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
